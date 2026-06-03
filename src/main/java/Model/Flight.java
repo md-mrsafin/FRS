2da0e57ffac92dd55af3c75123c8b1d57c28b4de
Safin <safin@gmail.com>
2026-06-03 22:43:28 +0600

    Flight model

---
+++ src/main/java/Model/Flight.java
@@ -0,0 +1,36 @@
+package Model;
+
+public class Flight extends CommonInFlight_User{
+
+    private String from;
+    private String to;
+    private String date;
+    private String time;
+    private int price;
+
+    public  Flight(String id,String name,int price,String from,String to,String date,String time){
+        this.name=name;
+        this.id=id;
+        this.price=price;
+        this.from=from;
+        this.to=to;
+        this.date=date;
+        this.time=time;
+
+    }
+
+
+    public String getTo(){
+        return this.to;
+    }
+    public String getDate(){
+        return this.date;
+    }
+    public String getTime(){
+        return this.time;
+    }
+    public int getPrice(){return this.price;}
+
+
+
+}
