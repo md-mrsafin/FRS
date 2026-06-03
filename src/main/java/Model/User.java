0a842f3613224a95a3cd04593f1bd9770af419e0
TABASSUM <afiajainab133@gmail.com>
2026-06-03 12:45:46 +0600

    User full model

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -34,6 +34,44 @@ public  class User extends CommonInFlight_User{
         this.age=age;
 
     }
+    public User(String email,String password){
+        this.email=email;
+        this.password=password;
+    }
+    public User(String name,String email,String password){
+        this.name=name;
+        this.email=email;
+        this.password=password;
+
+    }
+    public User(String name,String email,String phone,int age){
+        this.name=name;
+        this.email=email;
+        this.phone=phone;
+        this.age=age;
+
+    }
+    public String getName(){
+
+        return this.name;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getPhone(){
+
+        return this.phone;
+    }
+    public String getPassword(){
+
+        return this.password;
+    }
+    public int getAge(){
+
+        return this.age;
+    }
+
+
 
 
 }
