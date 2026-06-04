27c10c247df9c7c16aaff389d58373639f6dd9e7
TABASSUM <afiajainab133@gmail.com>
2026-06-04 15:32:52 +0600

    full booking model

--- src/main/java/Model/Booking.java
+++ src/main/java/Model/Booking.java
@@ -7,4 +7,40 @@ public class Booking {
     private String seat;
 
     private String flightId;
+
+
+    public Booking(){}
+
+    public Booking(String id, String userId, String flightId, String seat) {
+        this.id = id;
+
+        this.userId = userId;
+        this.flightId = flightId;
+        this.seat = seat;
+
+    }
+    public Booking( String userId, String flightId, String seat) {
+        this.userId = userId;
+        this.flightId = flightId;
+        this.seat = seat;
+
+    }
+    public String getId(){
+        return id;
+    }
+
+
+    public String getUserId(){
+        return userId;
+    }
+    public String getFlightId(){
+        return flightId;
+    }
+    public String getSeat(){
+        return seat;
+    }
+
+    public void setId(String id){
+        this.id=id;
+    }
 }
