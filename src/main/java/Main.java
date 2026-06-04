d1df268331de3ffb5334ace4469f1b39f2bb2913
Toriqul Islam <150716189+Toriqul2023@users.noreply.github.com>
2026-06-04 20:09:30 +0600

    Main api

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import Controller.bookingController;
+import Controller.flightController;
+import Controller.userController;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
-    }
+import com.sun.net.httpserver.HttpServer;
+
+
+import java.io.IOException;
+
+import java.net.InetSocketAddress;
+public class Main {
+    static void main(String[] args) throws IOEXception {
+    String portStr = System.getenv("PORT");
+        int port = (portStr != null) ? Integer.parseInt(portStr) : 9090;
+        HttpServer server= HttpServer.create(new InetSocketAddress(port),0);
+
+        server.createContext("/user",new userController());
+
+        server.createContext("/flight",new flightController());
+        server.createContext("/booking",new bookingController());
+
+        server.start();
+        System.out.println("Server started on port " + port);
+
+    
+    
+     }
+  }
+
 }
